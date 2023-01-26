0dbc6f6fb70f68c2a110accdaeb09ad8d341b25c
abdolazizsalimi <aziz.s6882@gmail.com>
2023-01-27 00:28:30 +0330

    [adding] dto pharmacy

--- src/module/pharmacy/dto/read-pharmacy.input.ts
+++ src/module/pharmacy/dto/read-pharmacy.input.ts
@@ -1,10 +1,30 @@
-import { ApiPropertyOptional } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
-import { IsOptional, ValidateNested } from 'class-validator';
+import { IsNumberString, IsOptional, IsString, ValidateNested } from 'class-validator';
 import { PaginationData } from 'src/common/input/pagination.input';
 import { SortByData } from 'src/common/input/sort-by.input';
 
-class ReadPharmacyData {}
+class ReadPharmacyData {
+
+  @ApiProperty()
+  @IsString()
+  pharmacy_name : string
+  @ApiProperty()
+  @IsString()
+  pharmacy_established_year : string
+  @ApiProperty()
+  @IsString()
+  pharmacy_address : string
+  @ApiProperty()
+  @IsString()
+  pharmacy_branch : string
+  @ApiProperty()
+  @IsNumberString()
+  pharmacy_phone_number : string 
+
+
+
+}
 
 export default class ReadPharmacyInput {
   @ApiPropertyOptional()
