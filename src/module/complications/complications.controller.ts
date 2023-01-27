8caecbfd18cdb29e0940603def5c4598a2b73ce8
abdolazizsalimi <aziz.s6882@gmail.com>
2023-01-27 21:33:33 +0330

    [adding] APIs

--- src/module/complications/complications.controller.ts
+++ src/module/complications/complications.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ComplicationsService } from './complications.service';
+import CreateComplicationsInput from './dto/creat-complications.input';
+import ReadComplicationsDataInput from './dto/read-complications.input';
 
 @Controller('complications')
-export class ComplicationsController {}
+export class ComplicationsController {
+    constructor(private complicationsService : ComplicationsService){}
+    @Get('readcomplications')
+    @ApiOperation({ operationId: 'readcomplications' })
+    @ApiBody({ type: ReadComplicationsDataInput })
+    @ApiResponse({ status: 200 })
+    async readComplications (input : ReadComplicationsDataInput){
+        await this.complicationsService.readComplications(input)
+    }
+
+
+
+
+    @Post('creatcomplications')
+    @ApiOperation({ operationId: 'creatcomplications' })
+    @ApiBody({ type: CreateComplicationsInput })
+    @ApiResponse({ status: 200 })
+    async craetComplications (input : CreateComplicationsInput){
+        await this.complicationsService.creatComplication(input)
+    }
+
+
+
+}
