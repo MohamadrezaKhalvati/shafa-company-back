ad4a71647b98263a3d9f1f430785651cf88f80ac
abdolazizsalimi <aziz.s6882@gmail.com>
2023-01-27 21:26:51 +0330

    [adding] APIs

--- src/module/suggestion/dto/read-suggestion.input.ts
+++ src/module/suggestion/dto/read-suggestion.input.ts
@@ -1,26 +1,44 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
-import { IsOptional, ValidateNested } from 'class-validator';
+import { IsOptional, IsString, ValidateNested } from 'class-validator';
 import { PaginationData } from 'src/common/input/pagination.input';
 import { SortByData } from 'src/common/input/sort-by.input';
 
-class ReadSuggestionData {}
+class ReadSuggestionData {
+
+
+  @ApiPropertyOptional()
+  @IsString()
+  suggestion_topic ?: string
+  
+  @ApiPropertyOptional()
+  @IsString()
+  suggestion_date ?: string 
+  
+  @ApiPropertyOptional()
+  @IsString()
+  suggestion_value ?: string
+
+
+
+
+}
 
 export default class ReadSuggestionInput {
   @ApiPropertyOptional()
   @IsOptional()
   @Type(() => PaginationData)
   @ValidateNested()
-  pagination: PaginationData;
+  pagination?: PaginationData;
 
   @ApiPropertyOptional()
   @IsOptional()
   @Type(() => SortByData)
-  sortBy: SortByData;
+  sortBy?: SortByData;
 
   @ApiPropertyOptional()
   @IsOptional()
   @Type(() => ReadSuggestionData)
   @ValidateNested()
-  data: ReadSuggestionData;
+  data?: ReadSuggestionData;
 }
--- src/module/suggestion/suggestion.service.ts
+++ src/module/suggestion/suggestion.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { createPaginationResult } from 'src/common/input/pagination.input';
 import { PrismaService } from '../prisma/prisma.service';
 import ReadSuggestionInput from './dto/read-suggestion.input';
 
@@ -7,6 +9,21 @@ export class SuggestionService {
   constructor(private prisma: PrismaService) {}
 
   async readSuggestion(input: ReadSuggestionInput) {
-    console.log(input);
+    
+    let whereClause: Prisma.suggestionsWhereInput = {
+      suggestions_topic : input.data.suggestion_topic || {},
+      suggestion_value : input.data.suggestion_value  || {},
+      suggestions_date  : input.data.suggestion_date  || {},
+
+
+   };
+   const count = this.prisma.suggestions.count({ where: whereClause });
+   const entity = this.prisma.suggestions.findMany({
+       where: whereClause,
+       ...input?.sortBy?.convertToPrismaFilter(),
+       ...input?.pagination?.convertToPrismaFilter(),
+   });
+   return createPaginationResult({ count, entity });
+    
   }
 }
